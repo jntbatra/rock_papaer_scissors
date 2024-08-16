2bb2767a9f3a0de4e39baebe9fc4c0fdedc617b0
Jayant Batra <jayantbtra@gmail.com>
2024-08-17 00:04:03 +0530

    Corrected Get computer choice

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
-//Get computers choice
-
-function computer_random(){
-    return (math.random(2)+1);
+function computer_random() {
+    return Math.floor(Math.random() * 3) + 1;
 }
 
-function computer_choice(computer_random){
-    switch(computer_random){
-    case(1):
-        return "Rock";
-        break;
-    case(2):
-        return "Paper";
-        break;
-    case(3):
-        return "Scissors";
-        break;
+function computer_choice(computer_random) {
+    switch(computer_random) {
+        case 1:
+            return "Rock";
+        case 2:
+            return "Paper";
+        case 3:
+            return "Scissors";
+    }
 }
-}
+
+const random_choice = computer_random();
+console.log(computer_choice(random_choice));
+
+
+
+//Get human choice
+
+
 
