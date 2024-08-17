d58e809a06f2414ff4268793c7e3e3f07913d8bf
Jayant Batra <jayantbtra@gmail.com>
2024-08-17 11:19:56 +0530

    Project complete

--- main.js
+++ main.js
@@ -14,8 +14,6 @@ function computer_choice(computer_random) {
     }
 }
 
-const random_choice = computer_random();
-console.log(computer_choice(random_choice));
 
 
 
@@ -40,13 +38,13 @@ function choice_number(human_choice){
         break;
 
         default:
+        console.log("You didn't enter ")
         return -1;
         break
     }
 }
 
-const entered_choice = human_choice();
-console.log(choice_number(entered_choice));
+
 
 
 //declareing score variable
@@ -54,6 +52,46 @@ console.log(choice_number(entered_choice));
 let score_h = 0;
 let score_c = 0;
 
+//logic to play single round
+
+function update_score(human,computer){
+    if(human == computer){
+        alert("Its a Draw");
+    }
+    else if((human ==  1 && computer == 2) || human == 2  && computer == 3 || human == 1 && computer == 3 )
+    {
+        alert ("Compter Won");
+        score_c ++;
+    }
+    else if ((human ==  2 && computer == 1) || human == 3  && computer == 2 || human == 3 && computer == 1)
+    {
+        alert ("Human Won");
+        score_h ++;
+    }
+}
 
 
+let score_h = 0;
+let score_c = 0;
 
+
+while(score_h < 5 || score_c < 5){
+    let entered_choice = human_choice();
+    choice_number(entered_choice);
+
+    let random_choice = computer_random();
+
+    update_score(entered_choice,random_choice);
+
+}
+
+
+if(score_h == 5)
+{
+    alert("Human You won the game Gr8");
+}
+
+else if(score_c == 5)
+{
+    alert ("Sorry Human, skill issue");
+}
